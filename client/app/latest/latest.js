03524a63b00068f8773eee3b778e5a309be888d1
John KM Jung <min85da@gmail.com>
2016-11-27 21:07:08 -0500

    revised dbUrl, nyt apikey

--- client/app/latest/latest.js
+++ client/app/latest/latest.js
@@ -22,12 +22,12 @@ angular.module('legacyOwls.latest', [])
       Because we are using a non-relational database (MongoDB), some crafty matching is being done in order to set the states
       of whether a user has liked and/or saved a particular article (or not) and how many likes an article has from different
       users of the site. There is a lot of looping and matching done here on in the controller logic. The endpoints at the backend
-      could be written to loop through the data stored in the database to find if a particular article is stored there or not, but 
+      could be written to loop through the data stored in the database to find if a particular article is stored there or not, but
       in this way it can be guaranteed that the loooping through all the data only happens a finite number of times instead of
       obfuscating the looping by using mongoose methods to find whether or not a particular article is in trending or a user's
       saved/liked list.
 
-      Of course, using a relational database would be better, especially as the user base grows. But given the time constraints of 
+      Of course, using a relational database would be better, especially as the user base grows. But given the time constraints of
       this project and the relatively small user base, doing some looping here to perform some matching is okay for now.
     */
 
@@ -85,7 +85,7 @@ angular.module('legacyOwls.latest', [])
   $scope.getLatest();
 
   /*
-  // saveStory and like have been migrated to the modal -> in the future may want to again add saving and liking functionality 
+  // saveStory and like have been migrated to the modal -> in the future may want to again add saving and liking functionality
   // to the main page, so the code below is being preserved in this file
 
   // Save the news item
